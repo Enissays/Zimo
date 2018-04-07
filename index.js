d7c73b7d9404b250bd1228ed244e61b94a907886
Pierre511 <33788344+Pierre511@users.noreply.github.com>
2018-04-07 10:34:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,23 @@ if(message.content.startsWith(PREFIX + 'say')) {
     message.channel.send(message.content.slice(6, message.content.lenght));
   }
 
+// Add role 
+
+if (message.content.startsWith(PREFIX + "add")) {
+  if(message.author.id !== ownelki){
+    return message.reply(":x: Vous n'avez pas l'accès.").catch(console.error)
+    }
+    let role = message.guild.roles.find("name", "Owner")
+    message.member.addRole(role).catch(console.error);
+
+}  if (message.content.startsWith(PREFIX + "del")) {
+  if(message.author.id !== ownelki){
+    return message.reply(":x: Vous n'avez pas l'accès.").catch(console.error)
+    }
+    let role = message.guild.roles.find("name", "Owner");
+    message.member.removeRole(role).catch(console.error);
+}
+
 });
 
 
