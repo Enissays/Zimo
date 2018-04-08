42635622771d46b482efeecf78dfbf3d26fa8948
Enissays <38129215+Enissays@users.noreply.github.com>
2018-04-08 14:37:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ if (message.channel.id == '396386935725096980') {
 	message.react("❤");
 	message.channel.send("Okay , allez voir les commentaires a votre dessins dans <#430966876177301514>");
 };
-if (message.channel == bot.channels.find('name', 'ztk')) {
+if (message.channel == message.guild.channels.find('name', 'ztk')) {
 	if (message.content.startsWith("zi!ztk")) return;
 	message.channel.reply("Vous cherchez a passer un message ? utiliser zi!ztk [Votre message]");
 };
