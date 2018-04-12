97bc21da5b84771ad6b4c3f2798fb74017498673
Enissays <38129215+Enissays@users.noreply.github.com>
2018-04-12 11:55:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -47,6 +47,11 @@ if (`${member.guild.id}` !== `362693232301441035`) return;
 
 
 });
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
 // Code Global
 
 function getFortune() {
